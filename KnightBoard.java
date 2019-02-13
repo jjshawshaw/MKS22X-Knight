2c26a4e8c124c205f5233957adf40e9792b73f8b
jjshawshaw <jshaw00@stuy.edu>
2019-02-13 11:23:26 -0500

    wrote constructor and clear method

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,21 +1,35 @@
-public KnightBoard{
+public class KnightBoard{
   private int[][] board;
 
 
   //@throws IllegalArgumentException when either parameter is <= 0.
-  public KnightBoard(int startingRows,int startingCols){}
+  public KnightBoard(int startingRows,int startingCols){
+      if (startingRows <= 0 || startingCols <= 0) throw new IllegalArgumentException();
+      board = new int[startingRows][startingCols];
+      clear();
+  }
+
+  public void clear(){
+    for (int y = 0; y < board.length; y ++){
+      for (int x = 0; x < board[0].length; x++){
+        board[y][x] = 0;
+      }
+    }
+  }
 
     /*blank boards display 0's as underscores
     you get a blank board if you never called solve or
     when there is no solution */
-  public String toString()
+
+//  public String toString()
 
 /*Modifies the board by labeling the moves from 1 (at startingRow,startingCol) up to the area of the board in proper knight move steps.
 @throws IllegalStateException when the board contains non-zero values.
 @throws IllegalArgumentException when either parameter is negative
  or out of bounds.
 @returns true when the board is solvable from the specified starting position*/
-public boolean solve(int startingRow, int startingCol)
+
+//public boolean solve(int startingRow, int startingCol)
 
 
 
@@ -23,10 +37,12 @@ public boolean solve(int startingRow, int startingCol)
 @throws IllegalArgumentException when either parameter is negative
  or out of bounds.
 @returns the number of solutions from the starting position specified*/
-public int countSolutions(int startingRow, int startingCol)
+
+//public int countSolutions(int startingRow, int startingCol)
 
 // level is the # of the knight
-private boolean solveH(int row ,int col, int level)
+
+//private boolean solveH(int row ,int col, int level)
 
 
 
