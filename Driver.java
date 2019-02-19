0cdf3acb5768efb54929f15152b58b2f11707b62
jjshawshaw <jshaw00@stuy.edu>
2019-02-18 22:22:27 -0500

    optimized countSolutions

--- Driver.java
+++ Driver.java
@@ -3,7 +3,7 @@ public class Driver{
   public static void main(String[]args){
     KnightBoard k = new KnightBoard(5, 5);
     System.out.println(k);
-    /*int i = 0;
+    int i = 0;
     for (int y = 0; y < 5; y ++){
       for (int x = 0; x < 5; x++){
         int s = k.countSolutions(y, x);
@@ -11,9 +11,9 @@ public class Driver{
         i += s;
       }
     }
-    System.out.println(i);*/
+    System.out.println(i);
     //System.out.println(k.numMovesString());
-    System.out.println(k.countSolutions(0, 0));
+    System.out.println(i);
     //k.solve(0, 2);
     //List<Tile> l = k.getMoves(0, 0);
     //for (int i = 0; i < l.size(); i++){
